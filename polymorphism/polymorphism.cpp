0af8603f6275076e5f31302a4aa055ec0078e22d
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-31 19:41:20 +0700

    memanggil program yangh sudah dibuat

--- polymorphism/polymorphism.cpp
+++ polymorphism/polymorphism.cpp
@@ -29,3 +29,18 @@ public:
 		cout << "Pesan dari lia" << endl;
 	}
 };
+
+int main() {
+	seseorang* orang;
+	joko a;
+	lia b;
+
+	orang = &a;
+	orang->pesan();
+	orang = &b;
+	orang->pesan();
+	//a,seseorang::pesan(); //memanggil method yang berada di class orang tua
+	//b.seseorang::pesan();
+
+	return 0;
+}
