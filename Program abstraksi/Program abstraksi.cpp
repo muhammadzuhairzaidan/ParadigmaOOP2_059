ec9898ef7f1d1b98fc10e546e12727c364d0f58c
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-31 20:38:52 +0700

    membuat class abstraksiklas dengan akses modifier private dan public dengan menambahkan fungsi

---
+++ Program abstraksi/Program abstraksi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+
+	// method untuk mengisi nilai
+	// private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	//menampilkan nilai
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+	}
+};
