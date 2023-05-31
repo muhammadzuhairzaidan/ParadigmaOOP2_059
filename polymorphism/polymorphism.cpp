1731dd40fac9894b963b05dbb245b71c0e6b3c06
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-31 19:21:21 +0700

    membuat class orang dengan akses modifier dan menambahkan fungsi

---
+++ polymorphism/polymorphism.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+	virtual void pesan() = 0;
+	//virtual void pesan() {
+		//cout << "Pesan dari seseorang" << endl;
+	//}
+	//virtual void cetak() = 0;
+	virtual void cetak() {
+
+	}
+};
