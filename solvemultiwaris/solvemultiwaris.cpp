6645a02b8c8e3e65389c047bba584bbfd7c4e70a
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-31 19:03:16 +0700

    membuat class pelajar turunan orang dengan akses modifier public

--- solvemultiwaris/solvemultiwaris.cpp
+++ solvemultiwaris/solvemultiwaris.cpp
@@ -21,3 +21,13 @@ public:
 		cout << "Pekerja dibuat\n" << endl;
 	}
 };
+
+class pelajar : virtual public orang {
+public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "Pelajar dibuat\n" << endl;
+	}
+};
