fa5b274035348b5f69a3b8f2be960c369be3afd8
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-31 21:01:15 +0700

    membuat class pelajar dari turunan manusia dan orang dengan akses modifier public dan menambahkan konstruktor dan destrukktor

--- Program MultiWaris/Program MultiWaris.cpp
+++ Program MultiWaris/Program MultiWaris.cpp
@@ -27,3 +27,21 @@ public:
 		cout << "manusia dihapus\n" << endl;
 	}
 };
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dengan jenis kelamin " + jenisKelamin + " dari sekolah " + sekolah + "\n\n";
+	}
+};
