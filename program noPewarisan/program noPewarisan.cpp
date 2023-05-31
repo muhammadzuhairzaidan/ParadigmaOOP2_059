42175b5ba70e4125d8eddd6a672e97be10a296b6
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-31 20:16:26 +0700

    membuat class deriviedClass turunan baseClass dengan akses public dengan menambahkan fungsi

--- program noPewarisan/program noPewarisan.cpp
+++ program noPewarisan/program noPewarisan.cpp
@@ -7,3 +7,10 @@ public:
 		cout << "Hallo saya Function dari base class";
 	}
 }; 
+
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
+	}
+};
