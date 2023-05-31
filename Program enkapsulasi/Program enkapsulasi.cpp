9b9c2a88403a9412976ae00110a2a49d30d85395
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-31 20:27:13 +0700

    program utama memanggil program yang sudah dibuat

--- Program enkapsulasi/Program enkapsulasi.cpp
+++ Program enkapsulasi/Program enkapsulasi.cpp
@@ -12,3 +12,19 @@ public:
 		return saklarNo[i];
 	}
 };
+
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+	lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getSaklarNo(0) << endl;
+	cout << lampuRumah.getSaklarNo(1) << endl;
+	cout << lampuRumah.getSaklarNo(2) << endl;
+	cout << lampuRumah.getSaklarNo(3) << endl;
+
+	return 0;
+}
