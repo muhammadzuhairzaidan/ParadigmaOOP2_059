ff94ead7646703b34ad3baa62dd0e43d9fd8f3f4
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-31 19:08:31 +0700

    membuat class budi turunan pekerja dan pelajar dengan akses modifier public

--- solvemultiwaris/solvemultiwaris.cpp
+++ solvemultiwaris/solvemultiwaris.cpp
@@ -31,3 +31,15 @@ public:
 		cout << "Pelajar dibuat\n" << endl;
 	}
 };
+
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur),
+		orang(pUmur)	//hal ini dapat dilakukan jika menggunakan virtual
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
