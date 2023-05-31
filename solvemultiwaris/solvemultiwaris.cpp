3fe0f6fd50db2544cf2bc152b9e472a139873738
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-31 18:51:23 +0700

    membuat class orang dengan akses modifier public

---
+++ solvemultiwaris/solvemultiwaris.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+	}
+};
+
